acccc3502ef36c77fbb7241c09d8f3de0e23a1ac
Yasir-Alii <binjabbar10@gmail.com>
2025-10-15 16:18:42 +0500

    Variables in python

---
+++ Foundations_Python/01_variables.py
@@ -0,0 +1,27 @@
+# 01_variables.py
+
+"""
+Topic: Variables in Python
+
+In Python, variables are used to store data.
+You don't need to declare a type — Python detects it automatically.
+"""
+
+# Example Variables
+name = "Yasir"          # String
+age = 21                # Integer
+height = 5.11           # Float
+is_student = True       # Boolean
+
+# Printing variables
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+# Updating a variable
+age = age + 1
+print("Next year, I will be:", age)
+
+# Combining text and variables
+print(f"Hello, my name is {name} and I am {age} years old.")
